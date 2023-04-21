29cb97aa204caa346989363018fc8fc4affa5e5b
stdkhai <k.o.zhuk@student.khai,edu>
2023-04-21 20:12:10 +0300

    m

---
+++ main.js
@@ -0,0 +1,37 @@
+const canvas = document.getElementById("canvas")
+canvas.height = window.innerHeight
+canvas.width = window.innerWidth
+
+const ctx = canvas.getContext("2d")
+
+let prevX = null
+let prevY = null
+
+ctx.lineWidth = 5
+
+let draw = false
+
+// Set draw to true when mouse is pressed
+window.addEventListener("mousedown", (e) => draw = true)
+// Set draw to false when mouse is released
+window.addEventListener("mouseup", (e) => draw = false)
+
+window.addEventListener("mousemove", (e) => {
+    // if draw is false then we won't draw
+    if(prevX == null || prevY == null || !draw){
+        prevX = e.clientX
+        prevY = e.clientY
+        return
+    }
+
+    let currentX = e.clientX
+    let currentY = e.clientY
+
+    ctx.beginPath()
+    ctx.moveTo(prevX, prevY)
+    ctx.lineTo(currentX, currentY)
+    ctx.stroke()
+
+    prevX = currentX
+    prevY = currentY
+})
